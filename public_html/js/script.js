ff4052724fcb7d33e19b234a85e0858762339ca6
Natan <natanrodrigues0044@gmail.com>
2021-12-01 19:53:46 -0300

    msg de confirmação

---
+++ public_html/js/script.js
@@ -0,0 +1,9 @@
+function fecharNav(){
+  document.getElementById("menuOculto").style.width="0";
+  document.getElementById("principal").style.marginLeft="0";
+}
+
+function abrirNav(){
+  document.getElementById("menuOculto").style.width="250px";
+  document.getElementById("principal").style.marginLeft="250px";
+}
